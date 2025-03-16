ba3a783458cc77ff4daa7c3f8147b4714641a5a1
vishnu <vishnurpillai2003@gmail.com>
2025-03-16 17:45:37 +0530

    routes for register and productList, registration includes hashing the product data and upload it to ipfs to generate cid which later added to qrHash which together got hashed for storing onto blockchain and finally generating the qrCode and saving the whole details in mongodb

---
+++ manufacturer-dashboard/backend/routes/productRoutes.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const crypto = require("crypto");
+const QRCode = require("qrcode");
+const Product = require("../models/Product");
+const authMiddleware = require("../middleware/authMiddleware");
+const { uploadToIPFS } = require("../utils/ipfsUtils");
+const { registerProductOnBlockchain } = require("../utils/blockchainUtils");
+
+const router = express.Router();
+
+// Register Product
+router.post("/register", authMiddleware, async (req, res) => {
+  try {
+    const { productName, serialNumber, batch } = req.body;
+    const companyName = req.user.companyName;
+  
+    // 1️⃣ Upload Data to IPFS
+    const productData = { productName, serialNumber, batch, companyName };
+    const qrhash = crypto.createHash("sha256").update(JSON.stringify(productData)).digest("hex");
+
+    const ipfsUploadData = {productData, qrhash}
+    const ipfsCID = await uploadToIPFS(JSON.stringify(ipfsUploadData));
+
+    const finalQRData = {ipfsCID, qrhash}
+    const finalQRHash = crypto.createHash("sha256").update(JSON.stringify(finalQRData)).digest("hex");
+    console.log(finalQRData)
+    
+    // 3️⃣ Store CID & Hash on Blockchain
+    try{
+      const txHash = await registerProductOnBlockchain(companyName,finalQRHash);
+    } catch (error) {
+      return res.status(400).json({ success: false, message: error.message });
+    }
+
+    // 4️⃣ Generate QR Code (Contains CID & Hash)
+    const qrContent = JSON.stringify({ ipfsCID, qrhash });
+    const qrCode = await QRCode.toDataURL(qrContent);
+
+    // 5️⃣ Save Product in Database
+    const newProduct = new Product({
+      productName,
+      serialNumber,
+      batch,
+      companyName,
+      ipfsCID,
+      qrhash,
+      qrCode,
+      manufacturer: req.user._id,
+    });
+
+    await newProduct.save();
+    res.json({ message: "Product registered successfully", ipfsCID, qrhash });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: "Error registering product", error: err.message });
+  }
+});
+
+router.get("/my", authMiddleware, async (req, res) => {
+  try {
+      const products = await Product.find({ manufacturer: req.user._id });
+      res.json(products);
+  } catch (error) {
+      console.error("Error fetching products:", error);
+      res.status(500).json({ success: false, message: "Server error!" });
+  }
+});
+
+module.exports = router;
